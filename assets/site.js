1d3c80de879081bbdc915036062c0d1b5e216bad
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-09-24 23:28:12 -0400

    still trying to fix firebase issues

--- assets/site.js
+++ assets/site.js
@@ -1,15 +1,15 @@
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyBAtwLdLN2_5wLIIbKE6yA4cxksUb3JQDY",
-    authDomain: "traintime-d6931.firebaseapp.com",
-    databaseURL: "https://traintime-d6931.firebaseio.com",
-    projectId: "traintime-d6931",
-    storageBucket: "traintime-d6931.appspot.com",
-    messagingSenderId: "503452863591",
-    appId: "1:503452863591:web:88782129b613056d3ad8c2"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+ var firebaseConfig = {
+  apiKey: "AIzaSyCmTCEFYX_KlkHwXM11dSW6rg439psiDdk",
+  authDomain: "train-homework2-6f7fa.firebaseapp.com",
+  databaseURL: "https://train-homework2-6f7fa.firebaseio.com",
+  projectId: "train-homework2-6f7fa",
+  storageBucket: "",
+  messagingSenderId: "349988934809",
+  appId: "1:349988934809:web:e4405a2558cfd4bf651408"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
   
   var database = firebase.database();
   var currentTime = moment();
